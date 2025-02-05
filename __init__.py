c198411d6f628b0b7a47a297d26070496e6215c9
dceleste35 <145990752+dceleste35@users.noreply.github.com>
2025-02-05 14:22:00 +0100

    Refactor import statement for ThreatDetector in __init__.py

--- __init__.py
+++ __init__.py
@@ -4,8 +4,7 @@ from flask import json
 from urllib.request import urlopen
 from werkzeug.utils import secure_filename
 import sqlite3
-
-threat_detector = ThreatDetector()
+from threat_detector import ThreatDetector
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'  # Clé secrète pour les sessions
